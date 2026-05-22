06e744409b890bbf14ddcf6884b1bf1a39c066da
GestaoProjetos2026 <cloudcomputing2026@unisanta.br>
2026-05-22 17:40:56 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -146,7 +146,8 @@ async function bootstrap() {
   });
 
   // await app.listen(port, '0.0.0.0');
-  await app.listen(process.env.PORT || 3000);
+  // await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 bootstrap();
