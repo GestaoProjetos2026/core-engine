cd6107e299282a36775fcbf79e75e544b5c0ff8e
Linkado36778 <barbosaa_lucas@hotmail.com>
2026-05-24 13:32:20 -0300

    updated api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '',
+  baseURL: import.meta.env.VITE_API_URL ||
+    'http://api-core-engine.40.82.176.176.nip.io/v1',
   headers: {
     'Content-Type': 'application/json',
   },
